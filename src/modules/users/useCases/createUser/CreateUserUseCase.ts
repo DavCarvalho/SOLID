52eb6a7425d1d93b4317978d87c03d844de26052
Davi Carvalho <davircsp@hotmail.com>
2022-06-19 16:50:04 -0300

    Projeto Finalizado

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
+
+  execute({ email, name }: IRequest): User {
+    const emailAlreadyExist = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyExist) {
+      throw new Error("Email already exist");
+    }
+
+    return this.usersRepository.create({ name, email });
+  }
+}
+
+export { CreateUserUseCase };
